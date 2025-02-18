e971bcad6c8da2d843f689c9ffdf2cfa20f7665b
Jairo Sepulveda C <30697670+infraestructura-it@users.noreply.github.com>
2025-02-17 23:11:38 -0500

    xx

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://bartoszlenar.github.io",
-  base: "/astro-milidev/",
+  site: "https://github.io/infraestructura-it/astro-milidev",
+  base: "/infraestructura-it/",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
